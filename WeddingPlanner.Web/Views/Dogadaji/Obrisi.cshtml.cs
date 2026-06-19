6e0018f4256cc43af1b4f3a5824ca37005de610a
iva <iva.cerkezl@fpmoz.sum.ba>
2026-06-19 13:50:31 +0200

    [Clan2] Dodane Views za Dogadaje

---
+++ WeddingPlanner.Web/Views/Dogadaji/Obrisi.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingPlanner.Web.Views.Dogadaji
+{
+    public class ObrisiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WeddingPlanner.Web/Views/Dogadaji/Uredi.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingPlanner.Web.Views.Dogadaji
+{
+    public class UrediModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
