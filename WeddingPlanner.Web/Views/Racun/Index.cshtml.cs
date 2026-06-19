2a795b2466ba2f866500cb522ae655e3456feaf6
iva <iva.cerkezl@fpmoz.sum.ba>
2026-06-19 14:00:31 +0200

    [Clan2] Dodane Views za Racun

---
+++ WeddingPlanner.Web/Views/Racun/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingPlanner.Web.Views.Racun
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
