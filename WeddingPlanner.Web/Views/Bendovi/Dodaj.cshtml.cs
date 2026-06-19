f74d6d89a1275345e79c8bf371330d1c44c132f7
iva <iva.cerkezl@fpmoz.sum.ba>
2026-06-19 13:39:58 +0200

    [Clan2] Dodane Views za Bendove

---
+++ WeddingPlanner.Web/Views/Bendovi/Dodaj.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingPlanner.Web.Views.Bendovi
+{
+    public class DodajModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WeddingPlanner.Web/Views/Bendovi/Uredi.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeddingPlanner.Web.Views.Bendovi
+{
+    public class UrediModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
